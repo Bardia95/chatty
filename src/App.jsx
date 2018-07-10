d5434fd79bb37fde1ce8defb088af3d09abdadf1
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-07-10 12:58:01 -0400

    refactored into react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import NavBar from './NavBar.jsx';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div className="container">
+        <NavBar />
+        <MessageList />
+        <ChatBar/>
+      </div>
     );
   }
 }
